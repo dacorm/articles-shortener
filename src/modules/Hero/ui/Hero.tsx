ff118ac3bef16631f5699f18c209f4b36b37a1ae
dacorm <stolen.youth@yandex.ru>
2023-05-26 22:59:20 +0700

    fix: url

--- src/modules/Hero/ui/Hero.tsx
+++ src/modules/Hero/ui/Hero.tsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <button
                     type='button'
                     onClick={() =>
-                        window.open("https://github.com/TidbitsJS/Summize", "_blank")
+                        window.open("https://github.com/dacorm/articles-shortener", "_blank")
                     }
                     className='black_btn'
                 >
